09f17698f08f67a3f9fd2b450dc99e6b5487447b
Mr-unver <71456743+Mr-unver@users.noreply.github.com>
2020-09-20 23:07:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+//Арифметика действительных чисел. Вычисление по формулам
+
+namespace Задание__2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //Console.Write("Введите числовое значение A: ");
+            //string a_first = Console.ReadLine();
+            //int a = Convert.ToInt32(a_first);
+            //Console.Write("Введите числовое значение B: ");
+            //string b_first = Console.ReadLine();
+            //int b = Convert.ToInt32(b_first);
+            //Console.WriteLine();
+            //Console.WriteLine($"Cумма:        a + b = {a + b}");
+            //Console.WriteLine($"Разница:      a - b = {a - b}");
+            //Console.WriteLine($"Произведение: a * b = {a * b}");
+            //Console.WriteLine();
+
+            //2
+            //Console.WriteLine("Введите числовое значение Х:");
+            //string inx = Console.ReadLine();
+            //double x = Convert.ToDouble(inx);
+            //Console.WriteLine("Введите числовое значение У:");
+            //string iny = Console.ReadLine();
+            //double y = Convert.ToDouble(iny);
+            //double result = (Math.Abs(x) - Math.Abs(y)) / (1 + Math.Abs(x * y));
+            //Console.WriteLine($"Х: {x}");
+            //Console.WriteLine($"У: {y}");
+            //Console.WriteLine($"Результат: {result:f2}");
+            //Console.WriteLine();
+
+            //3
+            //Console.WriteLine("Введите длину ребра куба:");
+            //string length = Console.ReadLine();
+            //int l = Convert.ToInt32(length);
+            //Console.WriteLine($"Объем указанного куба равен V: {Math.Pow(l, 3)}");
+
+            //4
+            //Console.WriteLine("Введите первое число:");
+            //string First = Console.ReadLine();
+            //double f = Math.Abs(Convert.ToDouble(First));
+            //Console.WriteLine("Введите второе число:");
+            //string Second = Console.ReadLine();
+            //double s = Math.Abs(Convert.ToDouble(Second));
+            //Console.WriteLine($"Среднее арифметическое: {(f + s) / 2}");
+            //Console.WriteLine($"Среднее геометрическое: {Math.Sqrt(f * s):f2}");
+
+            //5
+            //Console.WriteLine("Введите первое число:");
+            //string First = Console.ReadLine();
+            //double f = Convert.ToDouble(First);
+            //Console.WriteLine("Введите второе число:");
+            //string Second = Console.ReadLine();
+            //double s = Convert.ToDouble(Second);
+            //Console.WriteLine($"Среднее арифметическое: {(f + s) / 2}");
+            //Console.WriteLine($"Среднее геометрическое: {Math.Sqrt((Math.Abs(f) * Math.Abs(s))):f2}");
+        }
+    }
+}
